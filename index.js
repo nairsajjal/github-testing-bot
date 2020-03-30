9ccfd894b20200d9d161775a52f2e72377c09e3d
nairsajjal <nairsajjal@gmail.com>
2020-03-31 05:21:56 +0530

    added basic files

---
+++ index.js
@@ -0,0 +1,25 @@
+//importing jsonfile package
+const jsonfile = require('jsonfile');
+//import moment for date and timestamp
+const moment = require('moment');
+//adding simpleGit
+const simpleGit = require('simple-git');
+const FILE_PATH  = './data.json';//adding json file path
+
+const makeCommit = (x,y) => {
+    const DATE = moment().subtract(1,'y').add(1,'d')
+            .add(x,'w').add(y,'d').format(); //date obj
+    const data ={   //data obj
+        date: DATE
+    }
+    jsonfile.writeFile(FILE_PATH, data, ()=>{
+        simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+    });
+};
+
+
+
+
+
+makeCommit(25,46);
+
